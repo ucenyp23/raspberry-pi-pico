07a9bbd068ae9e936bda23bd68043dfee30af90a
ucenyp23 <ucenyp23@spstrutnov.cz>
2026-09-23 09:16:09 +0200

    Bus coms

--- Bus-communication/bus-communication.py
+++ Bus-communication/bus-communication.py
@@ -1,16 +1,20 @@
-from machine import Pin
-from utime import sleep
+from machine import Pin, UART
+from utime import sleep_ms
 
-pin = Pin("LED", Pin.OUT)
+uart = UART(1, baudrate=9600, tx=Pin(6), rx=Pin(7))
+button = Pin(0, Pin.IN, Pin.PULL_UP)
 
 def main():
+    previous_state = None
+    
     while True:
-        try:
-            pin.toggle()
-            sleep(1) # sleep 1sec
-        except KeyboardInterrupt:
-            break
-    pin.off()
+        current_state = button.value()
+        
+        if current_state != previous_state:
+            uart.write(str(current_state))
+            previous_state = current_state
+        
+        sleep_ms(50)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
